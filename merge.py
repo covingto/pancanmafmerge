038540d489760c592c714c62eb9bde5261910c96
Kyle R. Covington <kylecovington1@gmail.com>
2016-03-29 13:31:53 -0500

    Initial Commit

---
+++ merge.py
@@ -0,0 +1,206 @@
+
+
+##
+# "wrapper" utility for VCF merge and MAF generation
+
+import os, os.path, sys
+import tempfile, re
+import hgsc_vcf
+import subprocess
+import logging
+
+logger = logging.getLogger('merge')
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(stream=sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+
+PACKAGEDIR = os.path.dirname(os.path.abspath(__file__))
+
+def filter(fpath, caller, tmpdir):
+    outputfpath = os.path.join(tmpdir, os.path.splitext(os.path.basename(fpath))[0] + '.filtered.vcf')
+    if os.path.isfile(outputfpath):
+        logger.info("Skipping filtering because %s exists", outputfpath)
+        return outputfpath
+    if caller.lower() == 'muse':
+        logger.info("Applying MuSE filter to %s", fpath)
+        subprocess.check_call('python %(PACKAGE)s/filter_muse.py --level 5 %(input)s %(output)s' % {
+            'PACKAGE': PACKAGEDIR,
+            'input': fpath,
+            'output': outputfpath},
+            shell = True)
+        return outputfpath
+    else:
+        return fpath
+
+def sort(fpath, tmpdir):
+    outputfpath = os.path.join(tmpdir, os.path.splitext(os.path.basename(fpath))[0] + '.sorted.vcf')
+    if os.path.isfile(outputfpath):
+        logger.info("Skipping sort because %s exists", outputfpath)
+        return outputfpath
+    logger.info("Sorting %s -> %s", fpath, outputfpath)
+    subprocess.check_call('python %(PACKAGEDIR)s/vcf-sort.py %(seqdict)s %(input)s %(output)s' % {
+        'PACKAGEDIR': PACKAGEDIR,
+        'seqdict': '/hgsc_software/cancer-analysis/resources/references/human/hg19/hg19.dict',
+        'input': fpath,
+        'output': outputfpath},
+        shell = True)
+    return outputfpath
+
+##
+# returns a 2 tuple with the names of the genotype fields that correspond to the normal and tumor samples
+def getTNids(fpath):
+    sample_headers = []
+    field_header = None
+    with open(fpath, 'r') as fi:
+        for line in fi.readlines():
+            if line[0] != '#': 
+                break
+            if '##SAMPLE' == line[:8]:
+                sample_headers.append(line)
+            if '#CHROM' == line[:6]:
+                field_header = [c.strip() for c in line.split('\t')]
+    primaryline = [l for l in sample_headers if 'ID=PRIMARY' in l]
+    normalline = [l for l in sample_headers if 'ID=NORMAL' in l]
+    if primaryline:
+        primarysplit = hgsc_vcf.metainfo.ComplexHeaderLine(string = primaryline[0])
+    else:
+        raise ValueError("Could not find ID=PRIMARY in file %s, start again :)" % fpath)
+    if normalline:
+        normalsplit = hgsc_vcf.metainfo.ComplexHeaderLine(string = normalline[0])
+    else:
+        raise ValueError("Could not find ID=NORMAL in the file %s, start again :)" % fpath)
+    samples = field_header[9:]
+    if len(samples) < 2:
+        raise ValueError("Fewer than 2 samples found: %s" % samples)
+    elif len(samples) > 2:
+        raise ValueError("More than 2 samples found: %s" % samples)
+    # ok so we have the data that we need
+    if samples[0] == normalsplit.fields['SampleTCGABarcode']:
+        return samples[0], samples[1], normalsplit.fields['SampleTCGABarcode'], primarysplit.fields['SampleTCGABarcode']
+    elif samples[0] == 'NORMAL':
+        return samples[0], samples[1], normalsplit.fields['SampleTCGABarcode'], primarysplit.fields['SampleTCGABarcode']
+    elif samples[1] == normalsplit.fields['SampleTCGABarcode']:
+        return samples[1], samples[0], normalsplit.fields['SampleTCGABarcode'], primarysplit.fields['SampleTCGABarcode']
+    elif samples[1] == 'NORMAL':
+        return samples[1], samples[0], normalsplit.fields['SampleTCGABarcode'], primarysplit.fields['SampleTCGABarcode']
+    else:
+        raise ValueError("Can't figure out the tumor and normal sample id's in %s" % samples)
+
+def v2v(fpath, tmpdir):
+    outputfpath = os.path.join(tmpdir, os.path.splitext(os.path.basename(fpath))[0] + '.v2v.vcf')
+    if os.path.isfile(outputfpath):
+        logger.info("Skipping vcf reduction because %s exists", outputfpath)
+        return outputfpath
+    logger.info("vcf reduction of %s -> %s", fpath, outputfpath)
+    nid, tid, nbar, tbar = getTNids(fpath)
+    subprocess.check_call('/hgsc_software/perl/perl-5.16.2/bin/perl %(PACKAGEDIR)s/vcf2maf/vcf2vcf.pl --input-vcf %(input)s --output-vcf %(output)s --vcf-tumor-id %(tid)s --vcf-normal-id %(nid)s' % {
+        'PACKAGEDIR': PACKAGEDIR,
+        'input': fpath,
+        'output': outputfpath,
+        'tid': tid,
+        'nid': nid},
+        shell = True)
+    return outputfpath
+
+def merge(outfile, mergefiles):
+    outputfpath = outfile
+    if os.path.isfile(outputfpath):
+        logger.info("Skipping merge because %s exists", outputfpath)
+        return outputfpath
+    logger.info("Merging %s -> %s", mergefiles, outputfpath)
+    subprocess.check_call('python %(PACKAGEDIR)s/vcf-merge.py --keys %(keys)s --output %(output)s %(inputs)s' % {
+        'PACKAGEDIR': PACKAGEDIR,
+        'keys': ' '.join([c for c, f in mergefiles]),
+        'output': outputfpath,
+        'inputs': ' '.join([f for c, f in mergefiles])},
+        shell = True)
+    return outputfpath
+
+def annotate(fpath, tmpdir):
+    outputfpath = os.path.join(tmpdir, os.path.splitext(os.path.basename(fpath))[0] + '.annotated.vcf')
+    if os.path.isfile(outputfpath):
+        logger.info("Skipping annotation because %s exists", outputfpath)
+        return outputfpath
+    logger.info("Processing annotation of %s -> %s", fpath, outputfpath)
+    vepannotation = os.path.join(tmpdir, 'vepannotate.vcf')
+    if os.path.isfile(vepannotation):
+        logger.info("Skipping vep annotation because %s exists", vepannotation)
+    else:
+        logger.info("Processing vep annotation")
+        subprocess.check_call('export PERL5LIB=/hgsc_software/cancer-analysis/code/vep-82:/users/covingto/perl5/lib/perl5:$PERL5LIB && export PATH=/hgsc_software/cancer-analysis/code/vep-82/htslib:$PATH && /hgsc_software/perl/perl-5.16.2/bin/perl /hgsc_software/cancer-analysis/code/vep-82/ensembl-tools-release-82/scripts/variant_effect_predictor/variant_effect_predictor.pl --dir /hgsc_software/cancer-analysis/code/vep-82/cache/human/grch37 --format vcf --everything -i %(input)s -o %(output)s --cache --vcf --force_overwrite --check_existing --allow_non_variant --buffer_size 1000 --offline --fork 2' % {
+            'input': fpath,
+            'output': vepannotation},
+            shell = True)
+    subprocess.check_call('export JYTHONPATH=/hgsc_software/cancer-analysis/halotron/illumina/illumina_v0.0.2/halotron/site-packages && export CLASSPATH=/hgsc_software/cancer-analysis/code/javalib/sqlite-jdbc-3.8.11.2/sqlite-jdbc-3.8.11.2.jar:/hgsc_software/cancer-analysis/code/picard-tools-1.129/picard.jar:/hgsc_software/cancer-analysis/code/picard-tools-1.129/picard-lib.jar:/hgsc_software/cancer-analysis/code/picard-tools-1.129/htsjdk-1.129.jar:/hgsc_software/cancer-analysis/code/krcgtk/krcgtk-0.01.jar:$CLASSPATH && /stornext/snfs2/can/code/jython/jython-2.7.0/bin/jython -J-Xmx15g /hgsc_software/cancer-analysis/halotron/illumina/illumina_v0.0.2/halotron/illumina/annotation/annotate_vcf_cosmic.py --reference %(reference)s %(cosmic)s %(input)s %(valstatus)s %(output)s' % {
+        'reference': '/hgsc_software/cancer-analysis/resources/references/human/hg19/hg19.fa',
+        'cosmic': '/hgsc_software/cancer-analysis/resources/annotation-databases/cosmic/v71/CosmicCodingMuts.cnt3.vcf',
+        'input': vepannotation,
+        'valstatus': '/hgsc_software/cancer-analysis/resources/dbsnp/hg19/146/dbSNP_b146_GRCh37p13.valstatus.db',
+        'output': outputfpath},
+        shell = True)
+    logger.info("Processing %s complete", outputfpath)
+    return outputfpath
+
+def convert(opath, fpath):
+    outputfpath = opath
+    if os.path.isfile(outputfpath):
+        logger.info("Skipping conversion because %s exists", outputfpath)
+        return outputfpath
+    logger.info("Processing conversion of %s -> %s", fpath, outputfpath)
+    nid, tid, nbar, tbar = getTNids(fpath)
+    subprocess.check_call('/hgsc_software/perl/perl-5.16.2/bin/perl %(PACKAGEDIR)s/vcf2maf/vcf2maf.pl -no-annotate -input-vcf %(input)s -output-maf %(output)s -vcf-tumor-id %(tid)s -vcf-normal-id %(nid)s -tumor-id %(tbar)s -normal-id %(nbar)s -copythrough COSMIC,CENTERS,CONTEXT,DBVS' % {
+        'input': fpath,
+        'output': outputfpath,
+        'tid': tid,
+        'nid': nid,
+        'tbar': tbar,
+        'nbar': nbar,
+        'PACKAGEDIR': PACKAGEDIR},
+        shell = True)
+    return outputfpath
+
+def main(args):
+    if len(args.vcfs) != len(args.callers):
+        raise ValueError("vcfs and callers lengths are not the same")
+    if args.tmpdir:
+        if not os.path.isdir(args.tmpdir):
+            os.makedirs(args.tmpdir)
+    else:
+        args.tmpdir = tempfile.mkdtemp()
+
+    # generate the caller tuples
+    calls = zip(args.callers, args.vcfs)
+    # filter the vcf files
+    filters = [(c, filter(f, c, args.tmpdir)) for c, f in calls]
+    # sort the vcf files
+    sorts = [(c, sort(f, args.tmpdir)) for c, f in filters]
+    # v2v
+    v2vs = [(c, v2v(f, args.tmpdir)) for c, f in sorts]
+    # merge
+    merged = merge(os.path.join(args.tmpdir, 'merged.vcf'), v2vs)
+    # annotate
+    annotated = annotate(merged, args.tmpdir)
+    # vcf2maf
+    convert(args.OUTPUTMAF, annotated)
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--vcfs', type = str, nargs = '+', help = 'vcf file path(s)')
+    parser.add_argument('--callers', type = str, nargs = '+', help = 'caller keys, same len as vcfs and in same order')
+    parser.add_argument('--tmpdir', type = str, help = 'location of tmp directory for processing')
+    parser.add_argument('OUTPUTMAF', type = str, help = 'output file path for the merged MAF file')
+
+    args = parser.parse_args()
+
+    main(args)
+
+
+
---
+++ vcf-sort.py
@@ -0,0 +1,142 @@
+
+import os, os.path, sys
+import logging
+import hgsc_vcf
+logger = logging.getLogger()                                                               
+logger.setLevel(logging.DEBUG)                                                             
+ch = logging.StreamHandler()                                                               
+ch.setLevel(logging.DEBUG)                                                                 
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')      
+ch.setFormatter(formatter)                                                                 
+logger.addHandler(ch)                                                                      
+class SeqDict(object):
+    def __init__(self, f):
+        self.f = f
+        with open(f, 'r') as fi:
+            _struct = []
+            for line in fi.readlines():
+                _struct.append([c.strip() for c in line.split('\t')])
+            self._struct = _struct
+    
+    def contigs(self):
+        result = []
+        for s in self._struct:
+            result.append(s[1].split(':',2)[1])
+        return result
+
+class FileSplitter(object):
+    def __init__(self, reader, seqdict, nrecs = 10000):
+        self.reader = reader
+        self.seqdict = seqdict
+        self.nrecs = nrecs
+        self._fileindex = 0
+    
+    def _initialize(self, chrom):
+        self._fileindex += 1
+        writer = hgsc_vcf.Writer(open('%s.%s.split.vcf' % (chrom, str(self._fileindex)), 'w'), self.reader.header)
+        writer.write_header()
+        return writer
+
+    def split(self):
+        # start to split out the input file
+        # output pattern is chr.splitnumber.split.vcf
+        # all output files have the input header
+
+        # initialize all 
+        outfiles = {c:[] for c in self.seqdict.contigs()}
+        outcontainer = {c:self._initialize(c) for c in self.seqdict.contigs()}
+        outlines = {c:[] for c in self.seqdict.contigs()}
+        for record in self.reader:
+            c = record['CHROM']
+            if c not in outcontainer:
+                continue
+            outlines[c].append(record)
+                
+            if sum([len(v) for v in outlines.values()]) > self.nrecs:
+                cc, recs = sorted(outlines.items(), key = lambda x: len(x[1]))[-1]
+                logger.info("Max recs reached, dumping %s", cc)
+                outfiles[cc].append(outcontainer[cc].fobj.name)
+                for rec in sorted(recs, key = lambda x: int(x['POS'])):
+                    try:
+                        outcontainer[cc].write_record(rec)
+                    except:
+                        logger.error("Failed to write line %s", rec)
+                outcontainer[cc].fobj.close()
+                outcontainer[cc] = self._initialize(cc)
+                outlines[cc] = []
+        for c, recs in outlines.items():
+            writer = outcontainer[c]
+            outfiles[c].append(writer.fobj.name)
+            for rec in sorted(recs, key = lambda x: int(x['POS'])):
+                try:
+                    writer.write_record(rec)
+                except:
+                    logger.error("Failed to write line %s", rec)
+            writer.fobj.close()
+        return outfiles
+
+class FileMerger(object):
+    def __init__(self, writer, splitfiles, seqdict):
+        self.writer = writer
+        self.splitfiles = splitfiles
+        self.seqdict = seqdict
+
+    def merge(self):
+        for c in self.seqdict.contigs():
+            logger.info("Merging %s", c)
+            mergefiles = self.splitfiles[c]
+            self._merge_contig(mergefiles)
+            # merge is complete, remove the files
+            for f in mergefiles:
+                os.remove(f)
+
+    def position_compare(self, x, y):
+        return int(x.peek()['POS']) - int(y.peek()['POS'])
+
+    def _merge_contig(self, mergefiles):
+        holder = [hgsc_vcf.Reader(open(f, 'r')) for f in mergefiles]
+        for r in holder:
+            if r.peek() is None:
+                r.take() # will iterate if it can
+        while len(holder) > 1:
+            logger.info("Sorting")
+            holder = sorted([r for r in holder if r.peek() is not None], cmp=self.position_compare)
+            if len(holder) < 2:
+                break
+            h0 = holder[0]
+            h1 = holder[1]
+            while h0.peek() is not None and self.position_compare(h0, h1) < 1:
+                self.writer.write_record(h0.take())
+        last_r = holder[0]
+        while last_r.peek() is not None:
+            self.writer.write_record(last_r.take())
+
+def main(args):
+    # get the seqdict
+    seqdict = SeqDict(args.seqdict)
+
+    # split the file
+    splitter = FileSplitter(hgsc_vcf.Reader(open(args.input, 'r')), seqdict)
+    splitfiles = splitter.split()
+    splitter.reader.fobj.close()
+
+    # write the new files
+    merger = FileMerger(hgsc_vcf.Writer(open(args.output, 'w'), splitter.reader.header), splitfiles, seqdict)
+    merger.writer.write_header()
+    merger.merge()
+    merger.writer.fobj.close()
+
+    logger.info("Done")
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('seqdict', type = str, help = 'sequence dict file')
+    parser.add_argument('input', type = str, help = 'input file')
+    parser.add_argument('output', type = str, help = 'output file')
+
+    args = parser.parse_args()
+
+    main(args)
